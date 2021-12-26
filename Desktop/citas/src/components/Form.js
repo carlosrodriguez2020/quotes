fc2b77b78af4a0feb36c9e00f38bb6c6f93a6023
Carlos Rodriguez <carlosmvrodriguez9@gmail.com>
2021-12-26 19:58:40 -0300

    form was added into component form.js

--- Desktop/citas/src/components/Form.js
+++ Desktop/citas/src/components/Form.js
@@ -5,10 +5,45 @@ const Form = () => {
         <>
             <h2>Form</h2>
             <form>
-               
+                <label>Pet's name</label>
+                <input
+                type="text"
+                name="pet"
+                className="u-full-width"
+                placeholder="Kurama"
+                />
 
+                <label>Owner's name</label>
+                <input
+                type="text"
+                name="owner"
+                className="u-full-width"
+                placeholder="Naruto"
+                />
 
+                <label>Date</label>
+                <input
+                type="date"
+                name="date"
+                className="u-full-width"
+                />
 
+                <label>Hour</label>
+                <input
+                type="time"
+                name="time"
+                className="u-full-width"
+                />
+
+                <label>Symptom</label>
+                <textarea
+                type="text-area"
+                name="symptom"
+                className="u-full-width"
+                placeholder="Demon trapped inside a baby by the fourth hokage"
+                />
+
+                <button type="submit" className="u-full-width button-primary">Add appointment</button>
             </form>
         </>
     )}
